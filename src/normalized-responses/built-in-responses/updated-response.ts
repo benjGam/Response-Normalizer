57f9777b191111f97ac8145bd098f7eebb2876c1
benjGam <mecopilive@gmail.com>
2023-11-11 11:45:26 +0100

    feat: add UpdatedResponse class

---
+++ src/normalized-responses/built-in-responses/updated-response.ts
@@ -0,0 +1,18 @@
+import { HttpStatus } from '@nestjs/common';
+import { NormalizedResponseEntryObject } from 'structure-objects/normalized-response.object';
+import { ParsedExecContextObject } from 'structure-objects/parsed-exec-context.object';
+import NormalizedResponse from 'normalized-responses/normalized-response';
+
+export default class UpdatedResponse extends NormalizedResponse {
+  constructor(
+    parsedExecContext: ParsedExecContextObject,
+    data: any | any[],
+    entryObject: NormalizedResponseEntryObject = {
+      message:
+        '::subjectModuleName for ::stringifiedQueryParams has been updated',
+      statusCode: HttpStatus.OK,
+    },
+  ) {
+    super(parsedExecContext, data, entryObject);
+  }
+}
