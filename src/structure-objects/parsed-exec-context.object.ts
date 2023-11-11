1698b80bd8ccdc5198d02bbf3210879a4b485b8a
benjGam <mecopilive@gmail.com>
2023-11-11 10:48:54 +0100

    feat: add ParsedExecContextObject class

---
+++ src/structure-objects/parsed-exec-context.object.ts
@@ -0,0 +1,6 @@
+export class ParsedExecContextObject {
+  public logicModuleName: string;
+  public handlerModuleName: string;
+  public queryParams: Map<string, string>;
+  public httpMethod: string;
+}
