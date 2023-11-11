92f644e29d5dcd79937b396909fffd5eff2b54ec
benjGam <mecopilive@gmail.com>
2023-11-11 16:04:34 +0100

    docs: export external service decorator

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import { NormalizerInterceptor as InternalNormalizerInterceptor } from './normalizer.interceptor';
+import { ExternalService as InternalExternalService } from 'metadata-decorators/external-service.decorator';
 
 export const NormalizerInterceptor = InternalNormalizerInterceptor;
+export const ExternalService = InternalExternalService;
