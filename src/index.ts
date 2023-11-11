e47c1127c0069f22b61214ad16b188a753d76ca3
benjGam <mecopilive@gmail.com>
2023-11-11 16:13:06 +0100

    feat: export reflector interceptor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { NormalizerInterceptor as InternalNormalizerInterceptor } from './normalizer.interceptor';
-import { ExternalService as InternalExternalService } from 'metadata-decorators/external-service.decorator';
+import { ExternalService as InternalExternalService } from './metadata-decorators/external-service.decorator';
+import { default as RI } from './reflector.interceptor';
 
 export const NormalizerInterceptor = InternalNormalizerInterceptor;
 export const ExternalService = InternalExternalService;
+export const ReflectorInterceptor = RI;
