b90fab12f9a9effd03a814ed78ad680e161507c7
benjGam <mecopilive@gmail.com>
2023-11-12 10:31:23 +0100

    feat: add ExceptionResponse abstract class

---
+++ src/normalized-responses/built-in-responses/exception-response.ts
@@ -0,0 +1,21 @@
+import { HttpException } from '@nestjs/common';
+import NormalizedResponse from 'normalized-responses/normalized-response';
+import { NormalizedResponseEntryObject } from 'structure-objects/normalized-response.object';
+import { ParsedExecContextObject } from 'structure-objects/parsed-exec-context.object';
+
+export default abstract class ExceptionResponse extends NormalizedResponse {
+  constructor(
+    parsedExecContextObject: ParsedExecContextObject,
+    entryObject: NormalizedResponseEntryObject,
+  ) {
+    super(parsedExecContextObject, null, entryObject);
+    this.throw();
+  }
+
+  private throw() {
+    throw new HttpException(
+      this.normalizedResponseObject.message,
+      this.normalizedResponseObject.statusCode,
+    );
+  }
+}
