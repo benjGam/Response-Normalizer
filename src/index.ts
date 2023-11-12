88f8d7dba8a82f9c5a1069d2c7e80589d34d9a33
benjGam <mecopilive@gmail.com>
2023-11-12 14:40:25 +0100

    fix: use absolute path

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { NormalizerInterceptor as InternalNormalizerInterceptor } from './normal
 import { ExternalService as InternalExternalService } from './metadata-decorators/external-service.decorator';
 import { default as RI } from './reflector.interceptor';
 import { HttpExceptionFilter } from './http-exception.filter';
-import ResponseNormalizerOptions from 'configuration/response-normalizer.options';
+import ResponseNormalizerOptions from './configuration/response-normalizer.options';
 import { INestApplication } from '@nestjs/common';
 
 export const NormalizerInterceptor = InternalNormalizerInterceptor;
