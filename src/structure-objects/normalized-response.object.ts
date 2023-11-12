49ec619e276550b63252a8efc4d0c09355370338
benjGam <mecopilive@gmail.com>
2023-11-12 13:01:57 +0100

    feat: remove NormalizedResponseEntryObject

--- src/structure-objects/normalized-response.object.ts
+++ src/structure-objects/normalized-response.object.ts
@@ -1,13 +1,7 @@
 import { HttpStatus } from '@nestjs/common';
-import { OmitType } from '@nestjs/swagger';
 
 export class NormalizedResponseObject {
   public message: string;
   public data: any | any[];
   public statusCode: HttpStatus;
 }
-
-export class NormalizedResponseEntryObject extends OmitType(
-  NormalizedResponseObject,
-  ['data'],
-) {}
