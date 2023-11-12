a632df4035344c95cae06e4d5a7db3668b7cda8c
benjGam <mecopilive@gmail.com>
2023-11-12 14:21:29 +0100

    feat: add RNSMO class

---
+++ src/configuration/response-normalizer-success-messages.options.ts
@@ -0,0 +1,9 @@
+export default class ResponseNormalizerSuccessMessagesOptions {
+  public createdMessage?: string = '::subjectModuleName has been created';
+  public gettedMessage?: string =
+    '::subjectModuleName for ::stringifiedQueryParams has been getted';
+  public updatedMessage?: string =
+    '::subjectModuleName for ::stringifiedQueryParams has been updated';
+  public deletedMessage?: string =
+    '::subjectModuleName has been deleted for ::stringifiedQueryParams';
+}
