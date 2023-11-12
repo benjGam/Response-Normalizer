fd5a2f8feae289316f8fe84fbc03827eea194d59
benjGam <mecopilive@gmail.com>
2023-11-12 14:37:26 +0100

    feat: add configurator (test)

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,32 @@ import { NormalizerInterceptor as InternalNormalizerInterceptor } from './normal
 import { ExternalService as InternalExternalService } from './metadata-decorators/external-service.decorator';
 import { default as RI } from './reflector.interceptor';
 import { HttpExceptionFilter } from './http-exception.filter';
+import ResponseNormalizerOptions from 'configuration/response-normalizer.options';
+import { INestApplication } from '@nestjs/common';
 
 export const NormalizerInterceptor = InternalNormalizerInterceptor;
 export const ExternalService = InternalExternalService;
 export const ReflectorInterceptor = RI;
 
-export const init = function (nestApp: any) {
+export const init = function (
+  nestApp: any,
+  options?: ResponseNormalizerOptions,
+) {
   nestApp.useGlobalFilters(new HttpExceptionFilter());
+  new Configurator(nestApp, options);
 };
+
+export class Configurator {
+  public static options: ResponseNormalizerOptions;
+
+  constructor(nestApp: INestApplication, options?: ResponseNormalizerOptions) {
+    if (options === undefined) options = new ResponseNormalizerOptions();
+    Configurator.options = options;
+    this.init(nestApp);
+  }
+
+  private init(nestApp: INestApplication) {
+    if (Configurator.options.useNormalizerInterceptor)
+      nestApp.useGlobalInterceptors(new NormalizerInterceptor());
+  }
+}
