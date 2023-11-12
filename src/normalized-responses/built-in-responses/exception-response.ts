ffbc88ca34529f2b9c3721a84c312e7f207e88f7
benjGam <mecopilive@gmail.com>
2023-11-12 10:33:39 +0100

    fix: use absolute path instead of counting on node module resolution

--- src/normalized-responses/built-in-responses/exception-response.ts
+++ src/normalized-responses/built-in-responses/exception-response.ts
@@ -1,7 +1,7 @@
 import { HttpException } from '@nestjs/common';
-import NormalizedResponse from 'normalized-responses/normalized-response';
-import { NormalizedResponseEntryObject } from 'structure-objects/normalized-response.object';
-import { ParsedExecContextObject } from 'structure-objects/parsed-exec-context.object';
+import NormalizedResponse from '../../normalized-responses/normalized-response';
+import { NormalizedResponseEntryObject } from '../../structure-objects/normalized-response.object';
+import { ParsedExecContextObject } from '../../structure-objects/parsed-exec-context.object';
 
 export default abstract class ExceptionResponse extends NormalizedResponse {
   constructor(
