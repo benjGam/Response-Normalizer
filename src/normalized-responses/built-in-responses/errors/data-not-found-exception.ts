02ce9b97dc65ecb35e2ab9238884a3d7e1b45aa1
benjGam <mecopilive@gmail.com>
2023-11-12 14:59:12 +0100

    fix: use absolute paths

--- src/normalized-responses/built-in-responses/errors/data-not-found-exception.ts
+++ src/normalized-responses/built-in-responses/errors/data-not-found-exception.ts
@@ -1,7 +1,7 @@
 import ExceptionResponse from '../exception-response';
 import { ParsedExecContextObject } from 'structure-objects/parsed-exec-context.object';
 import { HttpStatus } from '@nestjs/common';
-import { Configurator } from 'configuration/configurator';
+import { Configurator } from '../../../configuration/configurator';
 
 export default class DataNotFoundExceptionResponse extends ExceptionResponse {
   constructor(
--- src/normalized-responses/built-in-responses/success/updated-response.ts
+++ src/normalized-responses/built-in-responses/success/updated-response.ts
@@ -1,7 +1,7 @@
 import { HttpStatus } from '@nestjs/common';
 import { ParsedExecContextObject } from '../../../structure-objects/parsed-exec-context.object';
 import NormalizedResponse from '../../normalized-response';
-import { Configurator } from 'configuration/configurator';
+import { Configurator } from '../../../configuration/configurator';
 
 export default class UpdatedResponse extends NormalizedResponse {
   constructor(
