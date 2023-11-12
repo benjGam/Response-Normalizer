67974629e2b09a9be31fa0b299d8702bec2c414e
benjGam <mecopilive@gmail.com>
2023-11-12 10:44:22 +0100

    feat: add DataNotFoundException response

---
+++ src/normalized-responses/built-in-responses/errors/data-not-found-exception.ts
@@ -0,0 +1,17 @@
+import { NormalizedResponseEntryObject } from 'structure-objects/normalized-response.object';
+import ExceptionResponse from '../exception-response';
+import { ParsedExecContextObject } from 'structure-objects/parsed-exec-context.object';
+import { HttpStatus } from '@nestjs/common';
+
+export default class DataNotFoundException extends ExceptionResponse {
+  constructor(
+    parsedExecContextObject: ParsedExecContextObject,
+    data: any | any[],
+    entryObject: NormalizedResponseEntryObject = {
+      message: 'No ::subjectModuleName found for ::stringifiedQueryParams',
+      statusCode: HttpStatus.NOT_FOUND,
+    },
+  ) {
+    super(parsedExecContextObject, data, entryObject);
+  }
+}
