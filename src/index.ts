150f3fefe347098bdf48338701ba68db743e73c8
benjGam <mecopilive@gmail.com>
2023-11-12 14:41:20 +0100

    feat: do not export interceptor

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,10 @@
-import { NormalizerInterceptor as InternalNormalizerInterceptor } from './normalizer.interceptor';
 import { ExternalService as InternalExternalService } from './metadata-decorators/external-service.decorator';
-import { default as RI } from './reflector.interceptor';
 import { HttpExceptionFilter } from './http-exception.filter';
 import ResponseNormalizerOptions from './configuration/response-normalizer.options';
 import { INestApplication } from '@nestjs/common';
+import { NormalizerInterceptor } from 'normalizer.interceptor';
 
-export const NormalizerInterceptor = InternalNormalizerInterceptor;
 export const ExternalService = InternalExternalService;
-export const ReflectorInterceptor = RI;
 
 export const init = function (
   nestApp: any,
