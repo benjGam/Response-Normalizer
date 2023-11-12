bc97ee44cce3ffd3cedbc5d535b8464344ca3fc4
benjGam <mecopilive@gmail.com>
2023-11-12 09:30:02 +0100

    feat: add StringifiedQueryParamsAliases

---
+++ src/helpers/aliases/stringified-query-params.alias.ts
@@ -0,0 +1,7 @@
+export default class StringifiedQueryParamsAliases {
+  public static getAliases() {
+    return {
+      stringifiedQueryParams: ['stringifiedqueryparams', 'queryparams', 'qp'],
+    };
+  }
+}
