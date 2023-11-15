56430440480f24f9fa2af022760bb406a8fe08da
benjGam <mecopilive@gmail.com>
2023-11-15 17:55:33 +0100

    feat: add formatingRule field

--- src/configuration/objects/query-params.options.ts
+++ src/configuration/objects/query-params.options.ts
@@ -1,3 +1,6 @@
+import FormattingRule from './formatting-rule';
+
 export default class QueryParamsOptions {
   public joinedBy?: string = 'and';
+  public formattingRules?: FormattingRule[] = [];
 }
