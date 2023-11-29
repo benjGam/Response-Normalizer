fc8bcf544fb046ec2dd16bf0f894bc759429c8f4
benjGam <mecopilive@gmail.com>
2023-11-29 12:30:53 +0100

    feat: export decorator

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { ExternalService as InternalExternalService } from './metadata-decorators/external-service.decorator';
 import { CustomResponseMessage as InternalCustomResponseMessage } from './metadata-decorators/custom-response-message.decorator';
+import { IgnoreFormattingRules as InternalIgnoreFormattingRules } from './metadata-decorators/ignore-formatting-rules.decorator';
 import { HttpExceptionFilter } from './http-exception.filter';
 import ResponseNormalizerOptions from './configuration/objects/response-normalizer.options';
 import { Configurator } from './configuration/configurator';
 
 export const ExternalService = InternalExternalService;
 export const CustomResponseMessage = InternalCustomResponseMessage;
+export const IgnoreFormattingRules = InternalIgnoreFormattingRules;
 
 export const init = function (
   nestApp: any,
