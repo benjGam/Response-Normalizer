6a15941b41a18b022ecf332da2ac3a24aa3b20e0
benjGam <mecopilive@gmail.com>
2023-11-29 12:13:22 +0100

    feat: add 'IgnoreFormattingRules' metadata decorator

---
+++ src/metadata-decorators/ignore-formatting-rules.decorator.ts
@@ -0,0 +1,5 @@
+import { SetMetadata } from '@nestjs/common';
+
+export const IGNORING_RULES = 'IGNORING_RULES';
+export const IgnoreFormattingRules = (rules: string[]) =>
+  SetMetadata(IGNORING_RULES, rules);
