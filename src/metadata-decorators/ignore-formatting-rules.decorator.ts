3edbd8f1a32409f452bdf0d7182106904da15e9a
benjGam <mecopilive@gmail.com>
2023-11-29 12:16:59 +0100

    feat: add default value to param

--- src/metadata-decorators/ignore-formatting-rules.decorator.ts
+++ src/metadata-decorators/ignore-formatting-rules.decorator.ts
@@ -1,5 +1,5 @@
 import { SetMetadata } from '@nestjs/common';
 
 export const IGNORING_RULES = 'IGNORING_RULES';
-export const IgnoreFormattingRules = (rules: string[]) =>
+export const IgnoreFormattingRules = (rules: string[] = []) =>
   SetMetadata(IGNORING_RULES, rules);
