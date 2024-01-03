47eb5e7da0cd22d0326c4a01678b946e6918e7ac
benjGam <mecopilive@gmail.com>
2024-01-03 15:28:38 +0100

    feat: export all from 'decorators'

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
-import { ExternalService as InternalExternalService } from './decorators/external-service.decorator';
-import { CustomResponseMessage as InternalCustomResponseMessage } from './decorators/custom-response-message.decorator';
-import { IgnoreFormattingRules as InternalIgnoreFormattingRules } from './decorators/ignore-formatting-rules.decorator';
-import { IgnoreNormalization as InternalIgnoreNormalization } from './decorators/ignore-normalization.decorator';
 import { HttpExceptionFilter } from './http-exception.filter';
 import ResponseNormalizerOptions from './configuration/objects/response-normalizer.options';
 import { Configurator } from './configuration/configurator';
 
-export const ExternalService = InternalExternalService;
-export const CustomResponseMessage = InternalCustomResponseMessage;
-export const IgnoreFormattingRules = InternalIgnoreFormattingRules;
-export const IgnoreNormalization = InternalIgnoreNormalization;
-
-export { DoNotThrowOnEmpty } from './decorators/do-not-throw-on-empty';
+export * from './decorators';
 
 export const init = function (
   nestApp: any,
