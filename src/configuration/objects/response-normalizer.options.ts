89370fdfcf58a86b078ab7a9d567ed029c8bee93
benjGam <mecopilive@gmail.com>
2024-01-03 15:57:05 +0100

    fix: adapt in top-level options object

--- src/configuration/objects/response-normalizer.options.ts
+++ src/configuration/objects/response-normalizer.options.ts
@@ -1,14 +1,10 @@
-import ExceptionMessagesOptions from './exception-messages.options';
+import MessagesOptions from './messages.options';
 import QueryParamsOptions from './query-params.options';
-import SuccessMessagesOptions from './success-messages.options';
 import ReflectorInterceptor from 'interceptors/reflector.interceptor';
 
 export default class ResponseNormalizerOptions {
   public useNormalizerInterceptor?: boolean = true;
-  public successMessages?: SuccessMessagesOptions =
-    new SuccessMessagesOptions();
-  public exceptionMessages?: ExceptionMessagesOptions =
-    new ExceptionMessagesOptions();
+  public messages?: MessagesOptions = new MessagesOptions();
   public queryParamsOptions?: QueryParamsOptions = new QueryParamsOptions();
   public includeStatusCode?: boolean = true;
   public ormInterceptor?: ReflectorInterceptor;
