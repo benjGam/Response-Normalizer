c4e1caa0313ca4ac7289100f2ddf785710473375
benjGam <mecopilive@gmail.com>
2024-01-03 14:46:55 +0100

    fix: set meta value as string instead of boolean

--- src/metadata-decorators/do-not-throw-on-empty.ts
+++ src/metadata-decorators/do-not-throw-on-empty.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const DO_NOT_THROW_ON_EMPTY = true;
+export const DO_NOT_THROW_ON_EMPTY = 'DO_NOT_THROW_ON_EMPTY';
 export const DoNotThrowOnEmpty = () => SetMetadata(DO_NOT_THROW_ON_EMPTY, true);
