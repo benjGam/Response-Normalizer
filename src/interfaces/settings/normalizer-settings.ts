21345c59a79851bf6c3c99e5be6b0e65e7c1bbab
benjGam <mecopilive@gmail.com>
2024-11-02 12:06:56 +0100

    feat: add optional flag on each fields for 'NormalizerSettings' interface

--- src/interfaces/settings/normalizer-settings.ts
+++ src/interfaces/settings/normalizer-settings.ts
@@ -4,8 +4,8 @@ import { QueryParameterFormatRule } from './rules/query-parameter-format.rule';
 import { QueryParameterRule } from './rules/query-parameter.rule';
 
 export interface NormalizerSettings {
-  responseMessages: Map<RequestMethod, MessageWrapper>;
-  queryParameterFormatRule: QueryParameterFormatRule;
-  queryParameterRules: Map<string, QueryParameterRule>;
-  includeStatusCode: boolean;
+  responseMessages?: Map<RequestMethod, MessageWrapper>;
+  queryParameterFormatRule?: QueryParameterFormatRule;
+  queryParameterRules?: Map<string, QueryParameterRule>;
+  includeStatusCode?: boolean;
 }
