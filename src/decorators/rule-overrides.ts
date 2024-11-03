26a96c429b9385534d378487e577dc9f0a3a32c2
benjGam <mecopilive@gmail.com>
2024-11-03 18:48:18 +0100

    feat: implement 'QueryParameterFormatRule' rule override

--- src/decorators/rule-overrides.ts
+++ src/decorators/rule-overrides.ts
@@ -1,12 +1,22 @@
 import { SetMetadata } from '@nestjs/common';
 import { MessageWrapper } from 'interfaces/settings/message-wrapper';
+import { QueryParameterFormatRule } from 'interfaces/settings/rules/query-parameter-format.rule';
 
 //Should be rework later.
 export class RuleOverrides {
   private static metadataKeys = new Map<string, string>([
     ['CustomMessage', 'RULE_OVERRIDE_CUSTOMMESSAGE'],
+    ['QueryParameterFormatRule', 'RULE_OVERRIDE_QUERYPARAMETERFORMATRULE'],
   ]);
 
   public static CustomMessage = (newMessageWrapper: Required<MessageWrapper>) =>
     SetMetadata(this.metadataKeys.get('CustomMessage'), newMessageWrapper);
+
+  public static QueryParameterFormatRule = (
+    newQueryParameterFormatRule: Required<QueryParameterFormatRule>,
+  ) =>
+    SetMetadata(
+      this.metadataKeys.get('QueryParameterFormatRule'),
+      newQueryParameterFormatRule,
+    );
 }
