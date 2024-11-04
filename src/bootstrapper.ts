a98ae637c15a30b0b56f3663e586b97a7c504d85
benjGam <mecopilive@gmail.com>
2024-11-04 10:31:40 +0100

    feat: implement 'getDefaultSettings' method to define base settings

--- src/bootstrapper.ts
+++ src/bootstrapper.ts
@@ -1,4 +1,6 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, RequestMethod } from '@nestjs/common';
+import { NormalizerInterceptor } from 'interceptors/normalizer-interceptor';
+import { MessageWrapper } from 'interfaces/settings/message-wrapper';
 import { NormalizerSettings } from 'interfaces/settings/normalizer-settings';
 
 export class Bootstrapper {
@@ -8,11 +10,56 @@ export class Bootstrapper {
   public static bootstrapNormalizer(
     app: INestApplication,
     settings: NormalizerSettings = undefined,
-  ): void {}
+  ): void {
+    app.useGlobalInterceptors(new NormalizerInterceptor());
+    if (!settings) {
+      this._settings = this.getDefaultSettings();
+    }
+  }
 
   private static fillUndefinedSettingValues(): void {}
 
   public static get settings(): NormalizerSettings {
     return this._settings;
+  }
+
+  private static getDefaultSettings(): NormalizerSettings {
+    return {
+      responseMessages: new Map<RequestMethod, MessageWrapper>([
+        [
+          RequestMethod.POST,
+          {
+            success: ':callSubject has been registered.',
+            failure: ':callSubject was already registered.',
+          },
+        ],
+        [
+          RequestMethod.GET,
+          {
+            success: ':callSubject :callQueryParams has been getted.',
+            failure: ':callSubject :callQueryParams was not found.',
+          },
+        ],
+        [
+          RequestMethod.DELETE,
+          {
+            success: ':callSubject :callQueryParams has been deleted.',
+            failure: ':callSubject :callQueryParams cannot be deleted.',
+          },
+        ],
+        [
+          RequestMethod.PATCH,
+          {
+            success: ':callSubject :callQueryParams has been updated.',
+            failure: ':callSubject :callQueryParams was not updated.',
+          },
+        ],
+      ]),
+      queryParameterFormatRule: {
+        syntax: "(':name': ':value')",
+        separator: ', ',
+      },
+      includeStatusCode: true,
+    };
   }
 }
