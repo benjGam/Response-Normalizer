66653faae349faa8a3d89cd41057aab30f0211f0
benjGam <mecopilive@gmail.com>
2024-11-04 10:02:51 +0100

    feat: add 'getOverride' & 'mergeOverrideWithRules' methods declarations

--- src/query-parameter-normalizer.ts
+++ src/query-parameter-normalizer.ts
@@ -4,6 +4,19 @@ import { QueryParameterRule } from 'interfaces/settings/rules/query-parameter.ru
 export class QueryParameterNormalizer {
   public normalize(context: DetailedContext): void {}
 
+  private getOverride(
+    context: DetailedContext,
+  ): Map<string, QueryParameterRule> {
+    return new Map<string, QueryParameterRule>();
+  }
+
+  //Have to be rework in UML
+  private mergeOverrideWithRules(
+    overrides: Map<string, QueryParameterRule>,
+  ): Map<string, QueryParameterRule> {
+    return new Map<string, QueryParameterRule>();
+  }
+
   private getRule(queryParameterName: string): QueryParameterRule {
     return null;
   }
