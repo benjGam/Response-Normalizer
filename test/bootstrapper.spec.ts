8876b94cf201b2df48cc01aec75c9fd6a8bdaffb
benjGam <mecopilive@gmail.com>
2024-11-06 08:54:51 +0100

    test: add test for default setting return

---
+++ test/bootstrapper.spec.ts
@@ -0,0 +1,45 @@
+import { RequestMethod } from '@nestjs/common';
+import { Bootstrapper } from '../src/bootstrapper';
+import { MessageWrapper } from '../src/interfaces/settings/message-wrapper';
+
+describe('Settings', () => {
+  test(`Should be the default settings`, () => {
+    expect(Bootstrapper['getDefaultSettings']()).toEqual({
+      responseMessages: new Map<RequestMethod, MessageWrapper>([
+        [
+          RequestMethod.POST,
+          {
+            success: ':callSubject has been registered.',
+            failure: ':callSubject was already registered.',
+          },
+        ],
+        [
+          RequestMethod.GET,
+          {
+            success: ':callSubject :callQueryParams has been getted.',
+            failure: ':callSubject :callQueryParams was not found.',
+          },
+        ],
+        [
+          RequestMethod.DELETE,
+          {
+            success: ':callSubject :callQueryParams has been deleted.',
+            failure: ':callSubject :callQueryParams cannot be deleted.',
+          },
+        ],
+        [
+          RequestMethod.PATCH,
+          {
+            success: ':callSubject :callQueryParams has been updated.',
+            failure: ':callSubject :callQueryParams was not updated.',
+          },
+        ],
+      ]),
+      queryParameterFormatRule: {
+        syntax: "(':name': ':value')",
+        separator: ', ',
+      },
+      includeStatusCode: true,
+    });
+  });
+});
