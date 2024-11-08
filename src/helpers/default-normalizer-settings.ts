4816d112a5a2157f0ba9d033224bc28c31b085a4
benjGam <mecopilive@gmail.com>
2024-11-08 13:26:46 +0100

    feat: implement 'defaultNormalizerSettings' object

---
+++ src/helpers/default-normalizer-settings.ts
@@ -0,0 +1,9 @@
+import { NormalizerSettings } from '../interfaces/settings/normalizer-settings';
+import { defaultQueryParameterFormat } from './default-query-parameter-format';
+import { defaultResponseMessages } from './default-response-messages';
+
+export const defaultNormalizerSettings: NormalizerSettings = {
+  responseMessages: defaultResponseMessages,
+  queryParameterFormatRule: defaultQueryParameterFormat,
+  includeStatusCode: true,
+};
