754c540574270319cb4ecb58e6a6c566a8545825
benjGam <mecopilive@gmail.com>
2024-11-27 00:40:59 +0100

    feat: implement 'QueryParameterNormalizerSettings' interface

---
+++ src/interfaces/settings/query-parameters.ts
@@ -0,0 +1,34 @@
+import { QueryParameterFormatRule, QueryParameterRule } from './rules';
+
+export interface QueryParameterNormalizerSettings {
+  /**
+   * This setting allows third-party developer to
+   * enable or disable case sensitive detection while
+   * `QueryParameterNormalizer` gets `queryParameterRules`.
+   *
+   * Example:
+   *
+   * `queryParameterRules` includes a record with ["uuid", {...}]
+   * if iterated parameter in normalization process is `UUID` then
+   * override will not be apply to iterated parameter.
+   *
+   * default: false
+   */
+  isParameterRulesIdentifiersCaseSensitive?: boolean;
+  /**
+   * This setting allows package to format
+   * sub string of query parameters
+   * in message field in responses.
+   *
+   * default: See `default-query-parameter-format`
+   */
+  queryParameterFormatRule?: QueryParameterFormatRule;
+  /**
+   * This setting allows third-party developer to
+   * replace http param names by others or apply
+   * a casing change on http param name.
+   *
+   * default: none
+   */
+  queryParameterRules?: Map<string, QueryParameterRule>;
+}
